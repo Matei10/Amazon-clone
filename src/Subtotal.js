c67b0b054054da7c7185dc67513a18f5e43cd564
Matei10 <88284753+Matei10@users.noreply.github.com>
2023-05-30 13:44:28 +0200

    update

---
+++ src/Subtotal.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./Subtotal.css";
+import {useStateValue} from './StateProvider';
+
+
+function Subtotal() {
+  const [{basket},dispatch]=useStateValue();
+  return (
+    <div className='subtotal'>
+        
+        
+        
+        
+
+            <p>
+
+                Subtotal ({basket.length} items):<strong>0</strong>
+            
+           </p>
+           <small className='subtotal_gift'>
+            <input type='checkbox'/>This order contains a gift
+            </small>
+            <button>Proceed to Checkout</button>
+            
+
+
+        </div>
+  )
+}
+
+export default Subtotal
